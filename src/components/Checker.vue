2a061395d326400a57247cbe6f193db7b6ecb9e1
judaitian <judaitian@tencent.com>
2022-02-09 11:26:23 +0800

    chore

--- src/components/Checker.vue
+++ src/components/Checker.vue
@@ -3,29 +3,34 @@
 </template>
 
 <script lang="ts">
-  import { defineComponent } from "vue";
-  export default defineComponent({
-    name: "HelloWorld",
-    setup: () => {
-      return {};
-    },
-  });
+import { defineComponent } from "vue";
+export default defineComponent({
+  name: "HelloWorld",
+  data() {
+    return {
+      counter: 0,
+    };
+  },
+  setup: () => {
+    return {};
+  },
+});
 </script>
 
 <style scoped>
-  a {
-    color: #42b983;
-  }
+a {
+  color: #42b983;
+}
 
-  label {
-    margin: 0 0.5em;
-    font-weight: bold;
-  }
+label {
+  margin: 0 0.5em;
+  font-weight: bold;
+}
 
-  code {
-    background-color: #eee;
-    padding: 2px 4px;
-    border-radius: 4px;
-    color: #304455;
-  }
+code {
+  background-color: #eee;
+  padding: 2px 4px;
+  border-radius: 4px;
+  color: #304455;
+}
 </style>
