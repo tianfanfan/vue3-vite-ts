b074ed18bdc2c953b63de344dea093ee0143188b
judaitian <judaitian@tencent.com>
2022-02-09 10:43:32 +0800

    ..

---
+++ .prettierrc.js
@@ -0,0 +1,13 @@
+module.exports = {
+  tabWidth: 2,
+  semi: true,
+  singleQuote: false,
+  printWidth: 80,
+  endOfLine: "auto",
+  quoteProps: "as-needed",
+  trailingComma: "all",
+  bracketSpacing: true,
+  jsxBracketSameLine: false,
+  arrowParens: "always",
+  // "vueIndentScriptAndStyle": true
+};
