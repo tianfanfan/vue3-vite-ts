36f135c08dfebb866c9dd17456239fb0874228fb
judaitian <judaitian@tencent.com>
2022-03-11 10:53:57 +0800

    chore tsconfig 包含 vite config

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import { defineConfig } from "vite";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/todo-list/',
+  base: "/todo-list/",
   plugins: [vue()],
   resolve: {
     alias: {
